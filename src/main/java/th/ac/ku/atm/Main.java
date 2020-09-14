5d0362e7e0c5c6cec49a4654fa6cdc98aed6cec6
twojiraporn <jiraporn.kow>
2020-09-14 18:53:26 +0700

    udpate main

--- src/main/java/th/ac/ku/atm/Main.java
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -1,11 +1,17 @@
 package th.ac.ku.atm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSourceDB dataSourceDB = new DataSourceDB();
-        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-        Bank bank = new Bank("My Bank", dataSourceFile);
-        ATM atm = new ATM(bank);
+
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+
+        ATM atm = context.getBean(ATM.class);
+
         AtmUI atmUI = new AtmUI(atm);
         atmUI.run();
     }
